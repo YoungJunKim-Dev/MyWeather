02924fdb4cda571e318cf270b6b9536ffbb1cab5
YJK <youngjunkim.dev@gmail.com>
2023-05-22 16:33:38 +0900

    weather app init

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from "expo-location";
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.dayText}>27</Text>
+          <Text style={styles.weatherName}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>27</Text>
+          <Text style={styles.weatherName}>Sunny</Text>
+        </View>
+      </ScrollView>
+      <Text>Temp!</Text>
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -13,8 +48,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1,
+    backgroundColor: "orange",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+  },
+  weather: {
+    backgroundColor: "skyblue",
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dayText: {
+    fontSize: 200,
+    fontWeight: "600",
+  },
+  weatherName: {
+    fontSize: 60,
   },
 });
