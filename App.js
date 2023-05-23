b3357909adce6bf8321001814fd10e33bd0a8449
YJK <youngjunkim.dev@gmail.com>
2023-05-23 17:41:16 +0900

    almost implemented

--- App.js
+++ App.js
@@ -1,28 +1,60 @@
 import * as Location from "expo-location";
 import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const WEATHER_API_KEY = "2834387742b25d5393a21e88fee8246a";
+  const GEO_API_KEY = "AIzaSyCBeW_-45ECOhQ1kucRA_yqdyKE-njE6zE";
+
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
   const [location, setLocation] = useState();
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
     }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    Location.setGoogleApiKey(GEO_API_KEY);
+    const loc = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(loc[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    const li = json.list;
+    console.log(typeof [1, 2, 3]);
+    const newli = li[0];
+    setDays(li);
+    console.log(newli);
+    console.log(days);
   };
 
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
 
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         horizontal
@@ -30,16 +62,22 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.dayText}>27</Text>
-          <Text style={styles.weatherName}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.dayText}>27</Text>
-          <Text style={styles.weatherName}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color="white" size="large" />
+          </View>
+        ) : (
+          days.map((day, index) => {
+            console.log(day);
+            return (
+              <View key={index} style={styles.day}>
+                <Text style={styles.dayText}>{day.dt_txt}</Text>
+                <Text style={styles.weatherName}>{day.weather[0].main}</Text>
+              </View>
+            );
+          })
+        )}
       </ScrollView>
-      <Text>Temp!</Text>
       <StatusBar style="dark" />
     </View>
   );
@@ -52,7 +90,7 @@ const styles = StyleSheet.create({
   },
   city: {
     flex: 1,
-    backgroundColor: "orange",
+    backgroundColor: "yellow",
     justifyContent: "center",
     alignItems: "center",
   },
@@ -69,7 +107,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   dayText: {
-    fontSize: 200,
+    fontSize: 80,
     fontWeight: "600",
   },
   weatherName: {
